8b92999fdc843872d39843a027c25435bb3417d9
Mon Maldonado <monterdi@gmail.com>
2017-03-07 12:07:53 +0100

    Initial commit

---
+++ HackerBooksAdv/HackerBooksAdv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerBooksAdv
+//
+//  Created by pigmonchu on 7/3/17.
+//  Copyright © 2017 digestiveThinking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
